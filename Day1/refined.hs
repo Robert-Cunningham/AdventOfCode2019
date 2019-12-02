29458a62372c97ec99400a9dcf6dccf7efc179a8
Robert Cunningham <robertvcunningham@gmail.com>
2019-12-01 20:08:09 -0500

    Add refined solution

---
+++ Day1/refined.hs
@@ -0,0 +1,15 @@
+main :: IO ()
+
+main = interact $ output . partb . input
+
+output = show
+input = map (read :: String -> Int) . lines
+
+fuel :: Integral a => a -> a
+fuel = subtract 2 . (`div` 3) --Why can you do this? Aren't we waiting on the first argument? Is this a special feature of infix notation, or is there some way to do this without it?
+
+parta :: Integral a => [a] -> a
+parta = sum . map fuel
+
+partb :: Integral a => [a] -> a
+partb ws = sum . map (sum . takeWhile (>= 0) . iterate fuel) $ ws
